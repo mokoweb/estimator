f49d688c2d4175b3c14a1573671b66635dfc97f2
Daniel Amoko <mokowebtech@gmail.com>
2020-04-09 13:04:58 +0100

    create challenge 1
    create currentlyInfectedSevere function, infectionsByRequestedTime function

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,22 @@
-const covid19ImpactEstimator = (data) => data;
+const currentlyInfectedImpact = ({ reportedCases }) => reportedCases * 10;
+
+const currentlyInfectedSevere = ({ reportedCases }) => reportedCases * 50;
+
+const infectionsByRequestedTime = (currentlyInfected, { timeToElapse }) => {
+  const factor = timeToElapse / 3;
+  return Math.trunc(currentlyInfected * (Math.power(2, factor)));
+};
+
+const covid19ImpactEstimator = (data) => ({
+  data, // the input data you got
+  impact: {
+    currentlyInfected: currentlyInfectedImpact(data),
+    infectionsByRequestedTime: infectionsByRequestedTime(this.currentlyInfected, data)
+  }, // your best case estimation
+  severeImpact: {
+    currentlyInfected: currentlyInfectedSevere(data),
+    infectionsByRequestedTime: infectionsByRequestedTime(this.currentlyInfected, data)
+  } // your severe case estimation
+});
 
 export default covid19ImpactEstimator;
