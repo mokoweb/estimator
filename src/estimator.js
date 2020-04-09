db300de360cbec9b128c68b9689d7aa4e73906f9
Daniel Amoko <mokowebtech@gmail.com>
2020-04-09 21:40:48 +0100

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -4,18 +4,18 @@ const currentlyInfectedSevere = ({ reportedCases }) => reportedCases * 50;
 
 const infectionsByRequestedTime = (currentlyInfected, { timeToElapse }) => {
   const factor = timeToElapse / 3;
-  return Math.trunc(currentlyInfected * (Math.power(2, factor)));
+  return Math.trunc(currentlyInfected * (2 ** factor));
 };
 
 const covid19ImpactEstimator = (data) => ({
   data, // the input data you got
   impact: {
     currentlyInfected: currentlyInfectedImpact(data),
-    infectionsByRequestedTime: infectionsByRequestedTime(this.currentlyInfected, data)
+    infectionsByRequestedTime: infectionsByRequestedTime(currentlyInfectedImpact(data), data)
   }, // your best case estimation
   severeImpact: {
     currentlyInfected: currentlyInfectedSevere(data),
-    infectionsByRequestedTime: infectionsByRequestedTime(this.currentlyInfected, data)
+    infectionsByRequestedTime: infectionsByRequestedTime(currentlyInfectedSevere(data), data)
   } // your severe case estimation
 });
 
